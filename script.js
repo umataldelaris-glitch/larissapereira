d334b699b49c1637886746092c02b4c83c5d56fe
umataldelaris-glitch <umataldelaris@gmail.com>
2026-08-23 22:07:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Interatividade do site da Larissa Pereira
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Site do Media Kit da Larissa Pereira carregado com sucesso!');
+});
