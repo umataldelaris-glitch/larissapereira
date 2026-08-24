d2dde3c1e2ab7872c281e5298f7449ceb78d2bdf
umataldelaris-glitch <umataldelaris@gmail.com>
2026-08-23 22:27:05 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Interatividade do site da Larissa Pereira
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Site do Media Kit da Larissa Pereira carregado com sucesso!');
-});
